c8d3e5acba79f02dde64082f0e631038524298a9
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-05-19 23:21:00 +0000

    implemented proper counter styling driven by jQuery toggleClass rather than direct css modification

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,7 +4,10 @@ $(document).ready(function() {
   $('#tweet-text').keyup(function() {
     let textArea = this;
     let count = $(textArea).val().length;
-    let counterViz = $(textArea).siblings().find('[name="counter"]');
+    let counterViz = $(textArea).siblings().find('.counter');
     $(counterViz).val(140 - count);
+
+    $(counterViz).toggleClass('error', count > 140);
   });
 });
+
