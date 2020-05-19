bd275169e4c3df852a380ea14406bf5143919612
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-05-19 22:23:09 +0000

    jquery selection now updating count live

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
   console.log('Document is ready now!');
 
-  $('#tweet-text').keyup(function(event) {
+  $('#tweet-text').keyup(function() {
     let textArea = this;
-    let counter = $(textArea).val().length;
-    console.log(counter);
+    let count = $(textArea).val().length;
+    let counterViz = $(textArea).siblings().find('[name="counter"]');
+    $(counterViz).val(140 - count);
   });
 });
