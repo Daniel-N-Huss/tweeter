3129717df1e19bb7645700eaac3cb1e60edf5fbc
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-05-20 21:39:42 +0000

    rendering tweets dynamically with jQuery / cleaned up templating code from index

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,19 +4,31 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png"
-    ,
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-};
 
 
 const calculateDisplayDate = function(postTime) {
@@ -60,19 +72,15 @@ const createTweetElement = function(data) {
   return $tweet;
 };
 
-const $tweet = createTweetElement(tweetData);
+// const $tweet = createTweetElement(tweetData);
 
-console.log($tweet);
-$('.tweet-container').append($tweet);
+// console.log($tweet);
+// $('.tweet-container').append($tweet);
 
+const renderTweets = function(tweetArray) {
+  tweetArray.forEach(tweet => {
+    $('.tweet-container').append(createTweetElement(tweet));
+  });
+};
 
-
-
-
-
-
-
-
-
-
-
+renderTweets(data);
